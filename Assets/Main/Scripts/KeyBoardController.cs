e5367688e2c8f3348e5a6db56eeb7a4cda59503e
Tasklog2 <tasklog2@gmail.com>
2019-04-19 12:47:32 +0900

    初コミット

---
+++ Assets/Main/Scripts/KeyBoardController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KeyBoardController : MonoBehaviour {
+
+    [SerializeField] private UnityEvent onUp, onDown, onRight, onLeft, onFire, onJump;
+
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown (KeyCode.UpArrow)) {
+            onUp.Invoke ();
+        }
+
+        if (Input.GetKeyDown (KeyCode.DownArrow)) {
+            onDown.Invoke ();
+        }
+
+        if (Input.GetKeyDown (KeyCode.RightArrow)) {
+            onRight.Invoke ();
+        }
+
+        if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+            onLeft.Invoke ();
+        }
+
+        if (Input.GetKeyDown (KeyCode.Space)) {
+            onFire.Invoke ();
+        }
+    }
+}
---
+++ Assets/Main/Scripts/PlayerControllerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerManager : MonoBehaviour {
+
+    [SerializeField] private GameObject playerObject;
+
+    void Start () {
+
+    }
+
+    public void PlayerMoveX (int value) {
+        var position = playerObject.transform.position;
+        position.x += value;
+
+        playerObject.transform.position = position;
+    }
+
+}
